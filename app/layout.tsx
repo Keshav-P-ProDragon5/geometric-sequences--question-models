779279dc7310c9670f136d18ab4254845c4bb1f6
Keshav-P-ProDragon5 <49ersarebetter49ers@gmail.com>
2025-10-15 10:15:15 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Geometric Sequences',
+  description: 'Created By:Keshav Paudel',
+  generator: 'None',
 }
 
 export default function RootLayout({
